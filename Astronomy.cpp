8b01a8fad680d7f37c9739235984bc08db3d4aea
Chris Spurgeon <chris@spurgeonworld.com>
2016-09-05 12:28:39 -0700

    Initial commit.

---
+++ Astronomy.cpp
@@ -0,0 +1,5 @@
+#include "Astronomy.h"
+
+Astronomy::Astronomy() {
+
+}
---
+++ Astronomy.h
@@ -0,0 +1,14 @@
+#ifndef Astronomy_included
+#define Astronomy_included
+
+
+
+class Astronomy {
+
+	public:
+		Astronomy();
+	private:
+};
+
+
+#endif
